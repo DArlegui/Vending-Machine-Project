3765840e0b04ede0420508535b6f6064ca419cc4
Nekodigi <38092328+Nekodigi@users.noreply.github.com>
2023-04-12 17:15:52 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import { Button, Typography } from "@mui/material";
+import "./App.css";
 
 function App() {
+  const [drinks, setDrinks] = useState([
+    { name: "Coke", price: 120, quantity: 10 },
+    { name: "Soda", price: 160, quantity: 10 },
+    { name: "Juice", price: 200, quantity: 10 },
+  ] as Drinks[]);
+  const [cash, setCash] = useState(500);
+
+  function getProduct(name: string) {
+    const product = drinks.findIndex((drink) => drink.name === name);
+    if (drinks[product].price <= cash) {
+      drinks[product].quantity -= 1;
+      setCash(cash - drinks[product].price);
+      setDrinks([...drinks]);
+    } else {
+      alert("Not enough money!");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography>Vending Machine</Typography>
+      <Typography>Cash: {cash}</Typography>
+      {drinks.map((drink) => {
+        return (
+          <Button variant="contained" onClick={() => getProduct(drink.name)}>
+            {drink.name} {drink.price} ({drink.quantity})
+          </Button>
+        );
+      })}
     </div>
   );
 }
+
+type Drinks = {
+  name: string;
+  price: number;
+  quantity: number;
+};
 
 export default App;
